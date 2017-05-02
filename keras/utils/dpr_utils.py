10e627f37f7fd561b7b19feac769659fc30cc99e
Karim Pedia <karim.pedia@gmail.com>
2017-05-02 03:37:39 +0200

    DPR minor

--- keras/utils/dpr_utils.py
+++ keras/utils/dpr_utils.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Apr 23 03:49:05 2017
-
 @author: ks
-
 Detection and Pattern Recognition Utilities
 """
 
@@ -14,43 +12,46 @@ import numpy as np
 
 
 def binary_metrics(binary_tp, binary_tn, binary_fp, binary_fn, beta=1.0):
-    _bb = beta ** 2
-    _eps = np.finfo(np.float32).eps
+    mt = {}
+    mt['binary_tp'], mt['binary_tn'], mt['binary_fp'], mt['binary_fn'], mt['beta'] = \
+        binary_tp, binary_tn, binary_fp, binary_fn, beta
+    bb = beta ** 2
+    _eps = mt['_eps'] = np.finfo(np.float32).eps
 
-    tp = true_positives = binary_tp
-    tn = true_negatives = binary_tn
-    fp = false_positives = binary_fp
-    fn = false_negatives = binary_fn
+    tp = mt['tp'] = mt['true_positives'] = mt['binary_tp']
+    tn = mt['tn'] = mt['true_negatives'] = mt['binary_tn']
+    fp = mt['fp'] = mt['false_positives'] = mt['binary_fp']
+    fn = mt['fn'] = mt['false_negatives'] = mt['binary_fn']
 
-    pp = bias = pred_positives = tp + fp
-    pn = pred_negatives = tn + fn
-    rn = real_negatives = tn + fp
-    rp = real_positives = prevalence = tp + fn
-    cr = class_ratio = rn / (rp + _eps)
+    pp = mt['pp'] = mt['bias'] = mt['pred_positives'] = tp + fp
+    pn = mt['pn'] = mt['pred_negatives'] = tn + fn
+    rn = mt['rn'] = mt['real_negatives'] = tn + fp
+    rp = mt['rp'] = mt['real_positives'] = mt['prevalence'] = tp + fn
+    mt['cr'] = mt['class_ratio'] = rn / (rp + _eps)
 
-    tpr = recall = sensitivity = hit_rate = tp / (rp + _eps)
-    tnr = inverse_recall = specificity = tn / (rn + _eps)
-    fpr = fall_out = fp / (rp + _eps)
-    fnr = miss_rate = fn / (rn + _eps)
+    tpr = mt['tpr'] = mt['_recall'] = mt['sensitivity'] = mt['hit_rate'] = tp / (rp + _eps)
+    mt['tnr'] = mt['inverse_recall'] = mt['specificity'] = tn / (rn + _eps)
+    mt['fpr'] = mt['fall_out'] = fp / (rp + _eps)
+    mt['fnr'] = mt['miss_rate'] = fn / (rn + _eps)
 
-    ppv = precision = confidence = positive_predictive_value = tp / (pp + _eps)
-    npv = inverse_precision = negative_predictive_value = tn / (pn + _eps)
-    fdr = false_discovery_rate = 1.0 - ppv
-    fOr = false_omission_rate = 1.0 - npv
+    ppv = mt['ppv'] = mt['_precision'] = mt['confidence'] = mt['positive_predictive_value'] = \
+        tp / (pp + _eps)
+    npv = mt['npv'] = mt['inverse_precision'] = mt['negative_predictive_value'] = tn / (pn + _eps)
+    mt['fdr'] = mt['false_discovery_rate'] = 1.0 - ppv
+    mt['_for'] = mt['false_omission_rate'] = 1.0 - npv
 
-    arithmetic_mean = ameasure = (tpr + ppv) / 2.0
-    geometric_mean = gmeasure = np.sqrt(tpr * ppv)
-    harmonic_mean = fmeasure = f1score = f1s = (2.0 * tpr * ppv / (tpr + ppv) if tp > 0 else 0)
-    fmeasure_2 = 2.0 * tp / (2.0 * tp + fp + fn) if tp > 0 else 0
-    fbs = fbeta = 0 if tp <= 0 else  (1 + _bb) * ppv * tpr / (_bb * ppv + tpr)
+    mt['arithmetic_mean'] = mt['ameasure'] = (tpr + ppv) / 2.0
+    mt['geometric_mean'] = mt['gmeasure'] = np.sqrt(tpr * ppv)
+    mt['harmonic_mean'] = mt['_fmeasure'] = mt['_f1score'] = mt['_f1s'] = \
+        (2.0 * mt['tpr'] * ppv / (tpr + ppv) if tp > 0 else 0)
+    mt['_fmeasure2'] = 2.0 * tp / (2.0 * tp + fp + fn) if tp > 0 else 0
+    mt['_fbs'] = mt['_fbeta'] = 0 if tp <= 0 else (1 + bb) * ppv * tpr / (bb * ppv + tpr)
 
-    acc_2 = accuracy_2 = binary_accuracy_2 = tp + tn
-    jcd = jaccard = tanimoto_similarity_coefficient = tp / (tp + fn + fp + _eps)
+    mt['_acc'] = mt['_accuracy'] = mt['_binary_accuracy'] = tp + tn
+    mt['jcd'] = mt['jaccard'] = mt['tanimoto_similarity_coefficient'] = tp / (tp + fn + fp + _eps)
 
-    auc = (tpr + tnr) / 2.0
-    informedness = tpr + tnr - 1
-    markedness = ppv + npv - 1
+    mt['auc'] = (tpr + mt['tnr']) / 2.0
+    mt['informedness'] = tpr + mt['tnr'] - 1
+    mt['markedness'] = ppv + npv - 1
 
-
-    return {k: v for k, v in locals().items() if not k.startswith('_')}
-
+    return mt
