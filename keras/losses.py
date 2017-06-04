6035ea428064091c0fc60cea5b276f2e53d90ca1
Karim Pedia <karim.pedia@gmail.com>
2017-06-04 08:24:42 +0200

    dice coef loss

--- keras/losses.py
+++ keras/losses.py
@@ -73,6 +73,20 @@ def cosine_proximity(y_true, y_pred):
     return -K.mean(y_true * y_pred, axis=-1)
 
 
+def dice_coef(y_true, y_pred, smooth=1):
+    """
+    Dice = (2*|X & Y|)/ (|X|+ |Y|)
+         =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
+    ref: https://arxiv.org/pdf/1606.04797v1.pdf
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (K.sum(K.square(y_true), -1) + K.sum(K.square(y_pred), -1) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -K.sum(y_true * y_pred, axis=-1) / (K.sum(y_true, axis=-1) + K.sum(y_pred, axis=-1))
+
+
 # Aliases.
 
 mse = MSE = mean_squared_error
@@ -81,6 +95,8 @@ mape = MAPE = mean_absolute_percentage_error
 msle = MSLE = mean_squared_logarithmic_error
 kld = KLD = kullback_leibler_divergence
 cosine = cosine_proximity
+bce = bCE = BCE = binary_crossentropy
+dcl = dCL = DCL = dice_coef_loss
 
 
 def serialize(loss):
